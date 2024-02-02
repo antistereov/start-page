8a7efce7a19e97c1998ca98378f6edf925e5b698
André Antimonov <andre.antimonov@posteo.de>
2024-02-02 19:00:15 +0100

    Configure Web Security using Basic Authentication

---
+++ src/main/kotlin/io/github/antistereov/start/config/WebSecurityConfig.kt
@@ -0,0 +1,23 @@
+package io.github.antistereov.start.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.invoke {
+            authorizeRequests {
+                authorize("/**", authenticated)
+            }
+            httpBasic {}
+        }
+        return http.build();
+    }
+}
