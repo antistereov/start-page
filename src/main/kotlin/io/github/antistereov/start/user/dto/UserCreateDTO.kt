c6a622401a05824f65d9bc11778f092859abea59
André Antimonov <andre.antimonov@posteo.de>
2024-02-03 12:47:09 +0100

    Add password validation

--- src/main/kotlin/io/github/antistereov/start/user/dto/UserCreateDTO.kt
+++ src/main/kotlin/io/github/antistereov/start/user/dto/UserCreateDTO.kt
@@ -2,6 +2,8 @@ package io.github.antistereov.start.user.dto
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 
 data class UserCreateDTO (
     @field:NotBlank(message = "Username is required.")
@@ -12,6 +14,11 @@ data class UserCreateDTO (
     val email: String,
 
     @field:NotBlank(message = "Name is required.")
+    @field:Size(min = 8, message = "Password must be between 8 and 20 characters.")
+    @field:Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}",
+        message = "Password must have at least one digit, " +
+                "one lowercase letter, one uppercase letter, " +
+                "one special character and no spaces.")
     val password: String,
 
     @field:NotBlank(message = "Password is required.")
