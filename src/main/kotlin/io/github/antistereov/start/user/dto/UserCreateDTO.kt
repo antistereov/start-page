114278ffc4a2b2fc32d706f74c2a4173f820f6d8
André Antimonov <andre.antimonov@posteo.de>
2024-02-09 10:46:20 +0100

    Make UserCreateDTO serializable and update password message.

--- src/main/kotlin/io/github/antistereov/start/user/dto/UserCreateDTO.kt
+++ src/main/kotlin/io/github/antistereov/start/user/dto/UserCreateDTO.kt
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class UserCreateDTO (
     @field:NotBlank(message = "Username is required.")
     val username: String,
@@ -14,7 +16,7 @@ data class UserCreateDTO (
     val email: String,
 
     @field:NotBlank(message = "Password is required.")
-    @field:Size(min = 8, message = "Password must be between 8 and 20 characters.")
+    @field:Size(min = 8, message = "Password must have at least 8 characters.")
     @field:Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}",
         message = "Password must have at least one digit, " +
                 "one lowercase letter, one uppercase letter, " +
