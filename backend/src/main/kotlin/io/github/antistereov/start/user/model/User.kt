901460ac8a368e1885378fe1ba274340c29ea5d8
André Antimonov <andre.antimonov@posteo.de>
2024-02-11 18:11:12 +0100

    Initialize reactive application

--- backend/src/main/kotlin/io/github/antistereov/start/user/model/User.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/user/model/User.kt
@@ -1,34 +1,18 @@
 package io.github.antistereov.start.user.model
 
 import jakarta.persistence.*
+import org.springframework.data.mongodb.core.mapping.Document
 
-@Entity
-@Table(name = "users")
-class User(
+@Document(collation = "users")
+data class User(
     @Id
     var id: String = "",
-
-    @Column(name = "spotify_access_token", length = 512)
     var spotifyAccessToken: String? = null,
-
-    @Column(name = "spotify_refresh_token", length = 512)
     var spotifyRefreshToken: String? = null,
-
-    @Column(name = "spotify_access_token_expiration_date")
     var spotifyAccessTokenExpirationDate: java.time.LocalDateTime? = null,
-
-    @Column(name = "todoist_access_token")
     var todoistAccessToken: String? = null,
-
-    @Column(name = "unsplash_access_token")
     var unsplashAccessToken: String? = null,
-
-    @Column(name = "nextcloud_url")
     var nextcloudHost: String? = null,
-
-    @Column(name = "nextcloud_username")
     var nextcloudUsername: String? = null,
-
-    @Column(name = "nextcloud_password")
     var nextcloudPassword: String? = null,
 )
