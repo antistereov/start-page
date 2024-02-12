369c3fdf3d25170df2453037cf4d68fe64ba108d
André Antimonov <andre.antimonov@posteo.de>
2024-02-12 02:57:22 +0100

    Remove redundant model imports in SpotifyService and GlobalExceptionHandler
    The redundant import statements for classes from "model" package have been removed in both SpotifyService and GlobalExceptionHandler. These classes were earlier relocated to "global.model" package improving overall code readability and indicating their wider scope within the project.

--- backend/src/main/kotlin/io/github/antistereov/start/widgets/spotify/service/SpotifyService.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/widgets/spotify/service/SpotifyService.kt
@@ -1,7 +1,6 @@
 package io.github.antistereov.start.widgets.spotify.service
 
 import io.github.antistereov.start.global.model.*
-import io.github.antistereov.start.model.*
 import io.github.antistereov.start.security.AESEncryption
 import io.github.antistereov.start.widgets.spotify.model.SpotifyTokenResponse
 import io.github.antistereov.start.user.repository.UserRepository
