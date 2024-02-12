c6980c12c0284ea82bc8c2af8acbe81475b20f03
André Antimonov <andre.antimonov@posteo.de>
2024-02-12 02:04:20 +0100

    Remove unnecessary dependencies from User model
    The User model file in the backend has been updated to enhance its readability and performance. The unnecessary dependencies, particularly those related to Jakarta, have been removed. This action also includes the removal of the ID annotation, reducing irrelevant code lines and making the User model cleaner and more efficient.

--- backend/src/main/kotlin/io/github/antistereov/start/user/model/User.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/user/model/User.kt
@@ -1,11 +1,9 @@
 package io.github.antistereov.start.user.model
 
-import jakarta.persistence.*
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "users")
 data class User(
-    @Id
     var id: String = "",
     var spotifyAccessToken: String? = null,
     var spotifyRefreshToken: String? = null,
