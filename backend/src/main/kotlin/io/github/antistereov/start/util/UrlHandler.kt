65f5ec15ca39d7b54ad943c04a439f26e5071a5b
André Antimonov <andre.antimonov@posteo.de>
2024-02-16 19:06:44 +0100

    Remove Copilot comments

--- backend/src/main/kotlin/io/github/antistereov/start/util/UrlHandler.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/util/UrlHandler.kt
@@ -7,14 +7,12 @@ import java.net.URL
 class UrlHandler {
 
     fun normalizeBaseUrl(baseUrl: String): String {
-        // Überprüfen, ob die URL gültig ist.
         val url = try {
             URL(baseUrl)
         } catch (e: Exception) {
             throw IllegalArgumentException("Provided string is not a valid URL")
         }
 
-        // Überprüfen, ob die URL mit "/" endet und diesen entfernen.
         return if (url.toString().endsWith("/")) {
             url.toString().substring(0, url.toString().length - 1)
         } else {
