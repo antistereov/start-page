50fb32b8e7f822ab443c924c3700ba14840d4545
André Antimonov <andre.antimonov@posteo.de>
2024-02-17 14:21:58 +0100

    Enable scheduling

--- backend/src/main/kotlin/io/github/antistereov/start/StartApplication.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/StartApplication.kt
@@ -2,8 +2,10 @@ package io.github.antistereov.start
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class StartApplication
 
 fun main(args: Array<String>) {
