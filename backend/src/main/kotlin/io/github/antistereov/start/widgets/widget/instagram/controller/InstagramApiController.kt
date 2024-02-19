a433a4c801d010a29748f8ae909effd10a44a020
André Antimonov <andre.antimonov@posteo.de>
2024-02-19 17:47:22 +0100

    Refactor InstagramApiController base route
    The base route in InstagramApiController class has been simplified to only include "/instagram" instead of "/widgets/instagram". This change simplifies the routing structure and provides clearer identification for the Instagram API controller.

--- backend/src/main/kotlin/io/github/antistereov/start/widgets/widget/instagram/controller/InstagramApiController.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/widgets/widget/instagram/controller/InstagramApiController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/widgets/instagram")
+@RequestMapping("/instagram")
 class InstagramApiController(
     private val apiService: InstagramApiService,
     private val principalExtractor: AuthenticationPrincipalExtractor,
