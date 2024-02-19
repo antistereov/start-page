7c3170e75ea1ec1863f60423ec9974d93bb475ba
André Antimonov <andre.antimonov@posteo.de>
2024-02-19 11:27:52 +0100

    Update UnsplashAuthDetails with recentPhotos attribute
    The UnsplashAuthDetails class in the user model is updated with a new attribute 'recentPhotos'. This attribute is a mutable list that will hold Photo objects, providing a way to keep a record of the recent photos associated with each Unsplash user. This enriches our user data model for improved functionality and user experience.

--- backend/src/main/kotlin/io/github/antistereov/start/user/model/UnsplashAuthDetails.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/user/model/UnsplashAuthDetails.kt
@@ -1,7 +1,11 @@
 package io.github.antistereov.start.user.model
 
+import io.github.antistereov.start.widgets.unsplash.model.Photo
+
+
 data class UnsplashAuthDetails(
     var userId: String? = null,
     var username: String? = null,
     var accessToken: String? = null,
+    var recentPhotos: MutableList<Photo> = mutableListOf()
 )
