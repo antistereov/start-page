fcc32f4effdf51d58e7e2219286b4cd6d86c07fd
André Antimonov <andre.antimonov@posteo.de>
2024-02-19 11:36:56 +0100

    Expand UnsplashApiController with new endpoints
    Added several new endpoints to UnsplashApiController along with their required service methods. These endpoints include getting a random photo, getting a photo's URL for a specific screen size, fetching recent photos, and fetching a specific recent photo's URL for a screen. This enhancement provides more options for photo retrieval, enhancing the functionality of the Unsplash API integration.

--- backend/src/main/kotlin/io/github/antistereov/start/widgets/unsplash/controller/UnsplashApiController.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/widgets/unsplash/controller/UnsplashApiController.kt
@@ -1,11 +1,13 @@
 package io.github.antistereov.start.widgets.unsplash.controller
 
 import io.github.antistereov.start.security.AuthenticationPrincipalExtractor
+import io.github.antistereov.start.widgets.unsplash.model.Photo
 import io.github.antistereov.start.widgets.unsplash.service.UnsplashApiService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
@@ -18,10 +20,55 @@ class UnsplashApiController(
     val logger: Logger = LoggerFactory.getLogger(UnsplashApiController::class.java)
 
     @GetMapping("/photo")
-    fun getRandomPhoto(@RequestParam query: String? = null): Mono<String> {
+    fun getRandomPhoto(authentication: Authentication, @RequestParam query: String? = null): Mono<Photo> {
         logger.info("Executing Unsplash getRandomPhoto with method query: ${query}.")
 
-        return apiService.getRandomPhoto(query)
+        return principalExtractor.getUserId(authentication)
+            .flatMap { userId ->
+                apiService.getRandomPhoto(userId, query)
+            }
+    }
+
+    @GetMapping("/photo/url")
+    fun getRandomPhotoUrlForScreen(
+        authentication: Authentication,
+        @RequestParam(required = false) query: String? = null,
+        @RequestParam(required = true) width: Int,
+        @RequestParam(required = true) height: Int,
+        @RequestParam(required = false) quality: Int = 85,
+    ): Mono<String> {
+        logger.info("Executing Unsplash getRandomPhotoUrlForScreen method with query: ${query}, width: ${width}, height: ${height}, quality: ${quality}.")
+
+        return principalExtractor.getUserId(authentication)
+            .flatMap { userId ->
+                apiService.getNewRandomPhotoUrlForScreen(userId, query, width, height, quality)
+            }
+    }
+
+    @GetMapping("/photo/recent")
+    fun getRecentPhotos(authentication: Authentication): Flux<Photo> {
+        logger.info("Executing Unsplash getRecentPhotos method.")
+
+        return principalExtractor.getUserId(authentication)
+            .flatMapMany { userId ->
+                apiService.getRecentPhotos(userId)
+            }
+    }
+
+    @GetMapping("/photo/recent/url")
+    fun getRecentPhotoUrlForScreen(
+        authentication: Authentication,
+        @RequestParam(required = true) index: Int,
+        @RequestParam(required = true) width: Int,
+        @RequestParam(required = true) height: Int,
+        @RequestParam(required = false) quality: Int = 85,
+    ): Mono<String> {
+        logger.info("Executing Unsplash getRecentPhotoUrlForScreen method with index: ${index}, width: ${width}, height: ${height}, quality: ${quality}.")
+
+        return principalExtractor.getUserId(authentication)
+            .flatMap { userId ->
+                apiService.getRecentPhotoUrlForScreen(userId, index, width, height, quality)
+            }
     }
 
     @GetMapping("/photo/{id}")
