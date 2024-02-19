ac4aaea4a9406a1538023f6f132442f12f5a13bc
André Antimonov <andre.antimonov@posteo.de>
2024-02-19 04:03:43 +0100

    Increase buffer size to 2MB

--- backend/src/main/kotlin/io/github/antistereov/start/config/WebClientConfig.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/config/WebClientConfig.kt
@@ -2,6 +2,8 @@ package io.github.antistereov.start.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ClientCodecConfigurer
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -9,11 +11,16 @@ class WebClientConfig {
 
     @Bean
     fun webClientBuilder(): WebClient.Builder {
+        val strategies = ExchangeStrategies.builder()
+            .codecs { configurer: ClientCodecConfigurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024) } // 2MB
+            .build()
+
         return WebClient.builder()
+            .exchangeStrategies(strategies)
     }
 
     @Bean
-    fun webClient(): WebClient {
-        return WebClient.create()
+    fun webClient(webClientBuilder: WebClient.Builder): WebClient {
+        return webClientBuilder.build()
     }
 }
