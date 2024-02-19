1033a257cf01da663859dd830d3c7ad2943d46d1
André Antimonov <andre.antimonov@posteo.de>
2024-02-19 20:35:29 +0100

    Remove username from InstagramAuthDetails data class
    The username field in the InstagramAuthDetails data class in the Instagram authentication model has been removed. This change simplifies the data class structure, as username information was not utilized in the Instagram authentication process.

--- backend/src/main/kotlin/io/github/antistereov/start/widgets/auth/instagram/model/InstagramAuthDetails.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/widgets/auth/instagram/model/InstagramAuthDetails.kt
@@ -2,7 +2,6 @@ package io.github.antistereov.start.widgets.auth.instagram.model
 
 data class InstagramAuthDetails(
     var userId: String? = null,
-    var username: String? = null,
     var accessToken: String? = null,
     var expirationDate: java.time.LocalDateTime? = null,
 )
