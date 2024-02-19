e9e50d12742fe62220d7d0250436da6bbcb66a4e
André Antimonov <andre.antimonov@posteo.de>
2024-02-19 11:19:51 +0100

    Add LoggerConfig file for basic logging setup
    A new LoggerConfig file is added in the backend source that will handle basic logging configuration. This will create a bean that will allow each class to get its own logger from LoggerFactory.

---
+++ backend/src/main/kotlin/io/github/antistereov/start/config/LoggerConfig.kt
@@ -0,0 +1,15 @@
+package io.github.antistereov.start.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class LoggerConfig {
+
+    @Bean
+    fun loggerForClass(clazz: Class<*>): Logger {
+        return LoggerFactory.getLogger(clazz)
+    }
+}
