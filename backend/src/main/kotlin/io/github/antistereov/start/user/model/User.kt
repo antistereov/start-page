c54a5eebbcb8c6bc79b7351bc3ee1aab17718b38
André Antimonov <andre.antimonov@posteo.de>
2024-02-29 11:54:56 +0100

    Update User model to include @Id annotation
    The User model has been refined in the backend to include the @Id annotation from Spring. This change is meant to better manage how user objects interact with the MongoDB database and also to improve the object mapping.

--- backend/src/main/kotlin/io/github/antistereov/start/user/model/User.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/user/model/User.kt
@@ -1,10 +1,11 @@
 package io.github.antistereov.start.user.model
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "users")
 data class User(
-    var id: String = "",
+    @Id val id: String,
     var auth: Auth = Auth(),
     var widgets: Widgets = Widgets(),
 )
