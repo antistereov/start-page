6a28cbb503311dee5705d9af16a0b3ca1095b880
André Antimonov <andre.antimonov@posteo.de>
2024-02-29 17:33:01 +0100

    Add logging to WeatherController and improve method naming
    The method "getWeatherWidgetSettings" in WeatherWidgetService.kt has been renamed to "getWeatherWidgetData" for better clarity. Also, logging has been added to all methods in WeatherController to aid tracking and debugging processes involving weather data updates and retrievals.

--- backend/src/main/kotlin/io/github/antistereov/start/widgets/widget/weather/controller/WeatherController.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/widgets/widget/weather/controller/WeatherController.kt
@@ -5,6 +5,7 @@ import io.github.antistereov.start.widgets.widget.weather.model.WeatherLocation
 import io.github.antistereov.start.widgets.widget.weather.model.WeatherWidgetData
 import io.github.antistereov.start.widgets.widget.weather.service.OpenWeatherMapService
 import io.github.antistereov.start.widgets.widget.weather.service.WeatherWidgetService
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
@@ -17,12 +18,16 @@ class WeatherController(
     private val principalExtractor: AuthenticationPrincipalExtractor,
 ) {
 
+    private val logger = LoggerFactory.getLogger(WeatherController::class.java)
+
     @GetMapping("/current")
     fun getCurrentWeather(
         authentication: Authentication,
         @RequestParam lat: Double,
         @RequestParam lon: Double,
     ): Mono<String> {
+        logger.info("Getting current weather for location: lat=$lat, lon=$lon")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
             widgetService.getCurrentWeather(userId, lat, lon)
         }
@@ -34,6 +39,8 @@ class WeatherController(
         @RequestParam lat: Double,
         @RequestParam lon: Double,
     ): Mono<String> {
+        logger.info("Getting weather forecast for location: lat=$lat, lon=$lon")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
             widgetService.getWeatherForecast(userId, lat, lon)
         }
@@ -46,15 +53,22 @@ class WeatherController(
         @RequestParam(required = false) countryCode: String?,
         @RequestParam(defaultValue = "5") limit: Int
     ): Mono<String> {
+        logger.info("Getting coordinates for location: $cityName" +
+                (if (stateCode != null) ", $stateCode" else "") +
+                if (countryCode != null) ", $countryCode" else ""
+        )
+
         return openWeatherMapService.getCoordinatesByLocationName(cityName, stateCode, countryCode, limit)
     }
 
     @GetMapping
-    fun getWeatherWidget(
+    fun getWeatherWidgetData(
         authentication: Authentication,
     ): Mono<WeatherWidgetData> {
+        logger.info("Getting weather widget data.")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
-            widgetService.getWeatherWidgetSettings(userId)
+            widgetService.getWeatherWidgetData(userId)
         }
     }
 
@@ -63,6 +77,8 @@ class WeatherController(
         authentication: Authentication,
         @RequestBody weatherLocation: WeatherLocation
     ): Mono<WeatherLocation> {
+        logger.info("Update primary location: ${weatherLocation.cityName}")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
             widgetService.updatePrimaryLocation(userId, weatherLocation)
         }
@@ -73,6 +89,8 @@ class WeatherController(
         authentication: Authentication,
         @RequestBody weatherLocation: WeatherLocation
     ): Mono<WeatherLocation> {
+        logger.info("Save location: ${weatherLocation.cityName}")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
             widgetService.saveWeatherLocation(userId, weatherLocation)
         }
@@ -84,6 +102,8 @@ class WeatherController(
         @RequestParam(required = true) lat: Double,
         @RequestParam(required = true) lon: Double,
     ): Mono<String> {
+        logger.info("Delete location: lat=$lat, lon=$lon")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
             widgetService.deleteWeatherLocation(userId, lat, lon)
         }
@@ -94,6 +114,8 @@ class WeatherController(
         authentication: Authentication,
         @RequestParam units: String
     ): Mono<String> {
+        logger.info("Update units: $units")
+
         return principalExtractor.getUserId(authentication).flatMap { userId ->
             widgetService.updateUnits(userId, units)
         }
