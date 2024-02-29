763a39ba095892ddf8c9956084634ac9c4706fb3
André Antimonov <andre.antimonov@posteo.de>
2024-02-29 17:27:22 +0100

    Refactor widget classes to widget data classes
    The widget classes - CalDavWidget, UnsplashWidget, and WeatherWidget - have been renamed to widget data classes - CalDavWidgetData, UnsplashWidgetData, and WeatherWidgetData respectively. All references to the old class names within Widgets.kt have been updated with the new class names, improving code clarity and organization.

--- backend/src/main/kotlin/io/github/antistereov/start/user/model/Widgets.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/user/model/Widgets.kt
@@ -1,12 +1,12 @@
 package io.github.antistereov.start.user.model
 
-import io.github.antistereov.start.widgets.widget.caldav.model.CalDavWidget
-import io.github.antistereov.start.widgets.widget.unsplash.model.UnsplashWidget
-import io.github.antistereov.start.widgets.widget.weather.model.WeatherWidget
+import io.github.antistereov.start.widgets.widget.caldav.model.CalDavWidgetData
+import io.github.antistereov.start.widgets.widget.unsplash.model.UnsplashWidgetData
+import io.github.antistereov.start.widgets.widget.weather.model.WeatherWidgetData
 
 data class Widgets(
-    var calDav: CalDavWidget = CalDavWidget(),
+    var calDav: CalDavWidgetData = CalDavWidgetData(),
     var chatId: String? = null,
-    var unsplash: UnsplashWidget = UnsplashWidget(),
-    var weather: WeatherWidget = WeatherWidget()
+    var unsplash: UnsplashWidgetData = UnsplashWidgetData(),
+    var weather: WeatherWidgetData = WeatherWidgetData()
 )
