dcfcc51cecb8ac14fc1566c30d22644b86a7c071
André Antimonov <andre.antimonov@posteo.de>
2024-02-29 16:02:41 +0100

    Refactor WeatherController to use WeatherWidgetService
    The WeatherController's methods have been revised to use the WeatherWidgetService, which centralizes weather-related functionality. This refactoring also included the integration of AuthenticationPrincipalExtractor for getting user's Auth details, allowing weather data to be user-specific.

--- backend/src/main/kotlin/io/github/antistereov/start/widgets/widget/weather/controller/WeatherController.kt
+++ backend/src/main/kotlin/io/github/antistereov/start/widgets/widget/weather/controller/WeatherController.kt
@@ -1,32 +1,42 @@
 package io.github.antistereov.start.widgets.widget.weather.controller
 
+import io.github.antistereov.start.security.AuthenticationPrincipalExtractor
+import io.github.antistereov.start.widgets.widget.weather.model.WeatherLocation
+import io.github.antistereov.start.widgets.widget.weather.model.WeatherWidget
 import io.github.antistereov.start.widgets.widget.weather.service.WeatherService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import io.github.antistereov.start.widgets.widget.weather.service.WeatherWidgetService
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/weather")
-class WeatherController(private val weatherService: WeatherService) {
+class WeatherController(
+    private val weatherService: WeatherService,
+    private val widgetService: WeatherWidgetService,
+    private val principalExtractor: AuthenticationPrincipalExtractor,
+) {
 
     @GetMapping("/current")
     fun getCurrentWeather(
+        authentication: Authentication,
         @RequestParam lat: Double,
         @RequestParam lon: Double,
-        @RequestParam(defaultValue = "metric") unit: String
     ): Mono<String> {
-        return weatherService.getCurrentWeather(lat, lon, unit)
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.getCurrentWeather(userId, lat, lon)
+        }
     }
 
     @GetMapping("/forecast")
     fun getWeatherForecast(
+        authentication: Authentication,
         @RequestParam lat: Double,
         @RequestParam lon: Double,
-        @RequestParam(defaultValue = "metric") unit: String
     ): Mono<String> {
-        return weatherService.getWeatherForecast(lat, lon, unit)
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.getWeatherForecast(userId, lat, lon)
+        }
     }
 
     @GetMapping("/location")
@@ -37,5 +47,55 @@ class WeatherController(private val weatherService: WeatherService) {
         @RequestParam(defaultValue = "5") limit: Int
     ): Mono<String> {
         return weatherService.getCoordinatesByLocationName(cityName, stateCode, countryCode, limit)
+    }
+
+    @GetMapping
+    fun getWeatherWidget(
+        authentication: Authentication,
+    ): Mono<WeatherWidget> {
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.getWeatherWidgetSettings(userId)
+        }
+    }
+
+    @PutMapping("/primary-location")
+    fun updatePrimaryLocation(
+        authentication: Authentication,
+        @RequestBody weatherLocation: WeatherLocation
+    ): Mono<WeatherLocation> {
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.updatePrimaryLocation(userId, weatherLocation)
+        }
+    }
+
+    @PostMapping("/location")
+    fun saveWeatherLocation(
+        authentication: Authentication,
+        @RequestBody weatherLocation: WeatherLocation
+    ): Mono<WeatherLocation> {
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.saveWeatherLocation(userId, weatherLocation)
+        }
+    }
+
+    @DeleteMapping("/location")
+    fun deleteWeatherLocation(
+        authentication: Authentication,
+        @RequestParam(required = true) lat: Double,
+        @RequestParam(required = true) lon: Double,
+    ): Mono<String> {
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.deleteWeatherLocation(userId, lat, lon)
+        }
+    }
+
+    @PutMapping("/units")
+    fun updateUnits(
+        authentication: Authentication,
+        @RequestParam units: String
+    ): Mono<String> {
+        return principalExtractor.getUserId(authentication).flatMap { userId ->
+            widgetService.updateUnits(userId, units)
+        }
     }
 }
