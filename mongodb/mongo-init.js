a957e8030c26207ffc5e9d6ae7490378789b4821
antistereov <admin@stereov.io>
2024-11-11 17:13:17 +0100

    Update(MongoDB): Refactor and remove unnecessary users

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -1,20 +1,2 @@
-var authDb = db.getSiblingDB('admin');
-
-authDb.createUser({
-    user: process.env.MONGO_READONLY_USERNAME,
-    pwd: process.env.MONGO_READONLY_PASSWORD,
-    roles: [
-        { role: 'read', db: 'start' },
-    ],
-});
-
-authDb.createUser({
-    user: process.env.MONGO_READWRITE_USERNAME,
-    pwd: process.env.MONGO_READWRITE_PASSWORD,
-    roles: [
-        { role: 'readWrite', db: 'start', },
-    ],
-});
-
 var startDb = db.getSiblingDB('start');
 startDb.createCollection("users");
